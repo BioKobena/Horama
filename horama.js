c58c09f8b0803f13b6b43e10c7edafec0d3c18d2
BioKobena <biokobena07@gmail.com>
2023-04-14 23:53:44 +0000

    Products parts

--- horama.js
+++ horama.js
@@ -2,7 +2,8 @@ const navbar = document.querySelector('.navbar');
 const navbarOffsetTop = navbar.offsetTop;
 const sections  = document.querySelectorAll('section');
 const navbarLinks = document.querySelectorAll('.navbar-link');
-const progress = document.querySelector('.progress-bars-wrapper');
+const container_nav = document.querySelector(".container-nav")
+// const progress = document.querySelector('.progress-bars-wrapper');
 window.addEventListener('scroll', ()=>{
     mainFn();
 });
@@ -11,10 +12,18 @@ const mainFn = () =>{
     if(window.pageYOffset >= navbar.offsetTop)
     {
        navbar.classList.add('sticky');
+       container_nav.style.backgroundColor = 'rgba(255, 255, 255, 0.3)'
+       container_nav.style.boxShadow = '0.1rem 0.1rem 0.1rem rgba(0, 0, 0, 0.2)';
+
     }
     else
     {
         navbar.classList.remove('sticky');
+       container_nav.style.backgroundColor = '#e8f5f6';
+     
+       container_nav.style.boxShadow = '0 0 0 rgba(0, 0, 0, 0.5)';
+
+
     }
 
     sections.forEach((section, i) => {
