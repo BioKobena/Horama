73812891d0365b7dc3c072f340ecd116bcedc4a2
BioKobena <biokobena07@gmail.com>
2023-04-15 10:56:05 +0000

    Update Sartuday

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function() {
     var loader = document.querySelector('.loader');
     setTimeout(() => {
       loader.style.display = 'none';
-    }, 500);
+    }, 2000);
     
   });
 
