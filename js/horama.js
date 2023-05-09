ed3274e622d0720dfbf1a5b3338d320f4acea8f8
BioKobena <biokobena07@gmail.com>
2023-05-09 10:31:05 -0700

    Finalisation & ThreeJS

--- js/horama.js
+++ js/horama.js
@@ -12,14 +12,19 @@ const mainFn = () =>{
     if(window.pageYOffset >= navbar.offsetTop)
     {
        navbar.classList.add('sticky');
-       container_nav.style.backgroundColor = 'rgba(255, 255, 255, 0.3)'
+       container_nav.style.backgroundColor = 'none'
        container_nav.style.boxShadow = '0.1rem 0.1rem 0.1rem rgba(0, 0, 0, 0.2)';
 
     }
     else
     {
-        navbar.classList.remove('sticky');
-       container_nav.style.backgroundColor = '#eee';
+       navbar.classList.remove('sticky');
+       container_nav.backgroundImage= "-webkit-gradient(linear, 0 0, 100% 100%, color-stop(0, #003073), color-stop(100%, #029797));"
+       container_nav.backgroundImage= "-webkit-linear-gradient(135deg, #003073, #029797);"
+       container_nav.backgroundImage= "-moz-linear-gradient(45deg, #003073, #029797);"
+       container_nav.backgroundImage= "-ms-linear-gradient(45deg, #003073 0, #029797 100%);"
+       container_nav.backgroundImage= "-o-linear-gradient(45deg, #003073, #029797);"
+       container_nav.backgroundImage= "linear-gradient(135deg, #003073, #029797);"
      
        container_nav.style.boxShadow = '0 0 0 rgba(0, 0, 0, 0.5)';
 
